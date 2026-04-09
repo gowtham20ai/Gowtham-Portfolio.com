e233264726b1af818adf455b609e9168233b0e21
gowtham <your@email.com>
2026-04-09 19:01:03 +0530

    new

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -149,7 +149,7 @@ const Skill = () => {
           </h1>
         </div>
         <center>
-       <a href="https://ik.imagekit.io/8wxvbajv2/Copy%20of%20Copy%20of%20White%20and%20Black%20Tech%20Professional%20Resume_20251223_103936_0000.png" download ><p className="py-2 px-3 w-fit text-center bg-gradient-to-l from-blue-500 via-teal-500 to-green-500 font-[Roboto] rounded-2xl
+       <a href="https://ik.imagekit.io/8wxvbajv2/Gowtham_Resume.pdf" download ><p className="py-2 px-3 w-fit text-center bg-gradient-to-l from-blue-500 via-teal-500 to-green-500 font-[Roboto] rounded-2xl
          " >Download Resume </p></a></center>
       </div>
     </div>
